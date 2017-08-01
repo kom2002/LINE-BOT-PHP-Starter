9fd8ccee3622fea45e5b8a4d26aa305b41780baa
kom2002 <tanakom@amivoicethai.com>
2017-08-01 15:24:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,47 +1,32 @@
 <?php
-$access_token = 'KtG6cn7tXPONQGU3OY4P4V+hC1MZl5Surcur2JjB7QkU+2eLXDpmJxCpV0F82RIZy+UxaR7eQ2OBFh90z/HTTr6YcHvWXrkIL2TGHfidKVuxY5d21Zzr0ddQ1xns9+sNjd6kvlV0Ccf4s7gA5TJ4WwdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' & $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'] . ' Powered by TrueVoice';
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+//Set Key
+$sSecretKey = 'ChatBot15chatBot';
+$json = '{
+"productid": "0887511204",
+"page": "invoicehistory",
+"channel": "chatbot",
+"lang": "th"
+}';
+$encrypt = fnEncrypt($json, $sSecretKey);
+echo $encrypt;
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+function fnEncrypt($sValue, $sSecretKey)
+{
+    $iv = base64_decode('Jc8EZ9nZPbWZEoV4ad8/CQ==');
+    $sValue = pkcs5_pad($sValue, mcrypt_get_block_size(MCRYPT_RIJNDAEL_128,
+    MCRYPT_MODE_CBC));
+    $sValue = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $sSecretKey, $sValue,
+    MCRYPT_MODE_CBC, $iv);
+    $sValue = base64_encode($sValue); return $sValue;
 }
-echo "OK";
+
+function pkcs5_pad($text, $blocksize) {
+    $pad = $blocksize - (strlen($text) % $blocksize); $pad2 = $text . str_repeat(chr($pad), $pad); return $pad2;
+}
+
+?>
+
